c1636b9428440762221729103b13e0d164e9eecb
Anik Chowdhury <54977516+Anik-Chowdhury@users.noreply.github.com>
2021-07-02 23:53:24 +0600

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -1,25 +1,16 @@
-'''
-I create 3 folder which one is 'pic' which download images from google drive... another 'edited pic' which add roll and name in template and last folder 'final pic' where final edited (tamplate+image+name + roll) picture stored.
-Note: download images from google drive only works one time because google block IP and give you corrupted image.
-the code will not work if there is any corrupted image in the 'pic' folder
-'''
-
-
-
-
-
 from pandas import *
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import requests
 import os
 
 # reading CSV file
-data = read_csv("frameCSV.csv")
+data = read_csv("responseCSV.csv")
 
 # converting column data to list
 Name = data['Name'].tolist()
 Student_Id = data['Student ID'].tolist()
 Upload_Your_decent_Photo = data['Upload Your decent Photo'].tolist()
+interest = data['Why do you want to join RMA?']
 img_list = []
 for i in Upload_Your_decent_Photo:
     j = i[33:]
@@ -27,9 +18,9 @@ for i in Upload_Your_decent_Photo:
 
 k = []
 for h in Student_Id:
-    g = 'F:/photoframe folder/pic/' + str(h) + '.png'
+    g = 'F:/last/pic/' + str(h) + '.png'
     k.append(g)
-
+'''
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
 
@@ -68,39 +59,43 @@ if __name__ == "__main__":
         destination = e
         download_file_from_google_drive(file_id, destination)
 
-
-arr = os.listdir('F:/photoframe folder/pic')
+'''
+arr = os.listdir('F:/last/pic')
 
 picinpicfolder = []
 for p in arr:
-    pp = 'F:/photoframe folder/pic/' + str(p)
+    pp = 'F:/last/pic/' + str(p)
     picinpicfolder.append(pp)
 
 finalpic = []
 for gigi in arr:
-    gii = 'F:/photoframe folder/finalpic/' + gigi[:-4] + '.png'
+    gii = 'F:/last/finalpic/' + gigi[:-4] + '.png'
     finalpic.append(gii)
 
 edited = []
 for r in Student_Id:
-    ss = 'F:/photoframe folder/editedpic/' + str(r) + '.png'
+    ss = 'F:/last/editedpic/' + str(r) + '.png'
     edited.append(ss)
 
-for o, p, real in zip(Name, Student_Id, edited):
+for o, p,inter, real in zip(Name, Student_Id, interest, edited):
 
-    mimage = Image.open('F:/photoframe folder/template.png')
+    mimage = Image.open('F:/last/Template-Empty.png')
 
-    font_type = ImageFont.truetype("arial.ttf", 70, encoding="unic")
-    font_type1 = ImageFont.truetype("arial.ttf", 70, encoding="unic")
+    font_type = ImageFont.truetype("arialbd.ttf", 80, encoding="unit8")
+    font_type1 = ImageFont.truetype("arialbd.ttf", 80, encoding="unit8")
+    font_type2 = ImageFont.truetype("arialbd.ttf", 30, encoding="unit8")
     text1 = o
     w, h = font_type.getsize(text1)
     draw = ImageDraw.Draw(mimage)
     roll = str(p)
-    draw.text(xy=((1000 - w) / 2, 800), text=text1, fill=(255, 69, 0), font=font_type)
-    draw.text(xy=(350, 900), text=roll, fill=(255, 69, 0), font=font_type1)
+    text2= str(inter)
+    draw.text(xy=(100, 1600), text=text1,  fill=(255, 255, 255), font=font_type)
+    draw.text(xy=(100, 1685), text=roll, fill=(255, 255, 255), font=font_type1)
+    draw.text(xy=(100, 1800), text=text2, fill=(255, 255, 255), font=font_type2)
+
 
     mimage.save(real)
-
+'''
 lis = []
 for gig in arr:
     gi = int(gig[:-4])
@@ -116,12 +111,12 @@ for li in lisi:
     for my in editr:
 
         if li == my:
-            kkk = 'F:/photoframe folder/pic/{}.png'.format(str(li))
-            fff = 'F:/photoframe folder/editedpic/{}.png'.format(str(my))
-            mmm = 'F:/photoframe folder/finalpic/{}.png'.format(str(li))
+            kkk = 'F:/last/pic/{}.png'.format(str(li))
+            fff = 'F:/last/editedpic/{}.png'.format(str(my))
+            mmm = 'F:/last/finalpic/{}.png'.format(str(li))
             secmain = Image.open(kkk)
             image = Image.open(fff)
-            secmain = secmain.resize((600, 600))
+            secmain = secmain.resize((1150, 1150))
             bigsize = (secmain.size[0] * 3, secmain.size[1] * 3)
             mask = Image.new('L', bigsize, 0)
             draw = ImageDraw.Draw(mask)
@@ -132,5 +127,6 @@ for li in lisi:
             output = ImageOps.fit(secmain, mask.size, centering=(0, 0))
             output.putalpha(mask)
 
-            image.paste(secmain, (190, 170), secmain)
+            image.paste(secmain, (753, 135), secmain)
             image.save(mmm)
+'''
